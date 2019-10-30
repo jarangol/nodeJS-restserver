e9efad9073956f34b0cf938ecf1339ec03c9e95a
Julian Arango <jarangolp@gmail.com>
2019-10-30 00:11:05 -0500

    moving mongo_uri as environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/nodejs-course';
 } else {
-    urlDB = 'mongodb+srv://julian:fTaJ9eH6osDgHGAq@cluster0-v4mmy.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
